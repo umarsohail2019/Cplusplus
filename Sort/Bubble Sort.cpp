0dd4b4da846a9347ddc361843fc7c001963bbdc3
Umar Sohail <umarsohail1357@gmail.com>
2019-06-18 16:51:50 +0500

    Files

--- Sort/Bubble Sort.cpp
+++ Sort/Bubble Sort.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <iterator>
+#include <random>
+#include <time.h>
 using namespace std;
+
+void initialize_array(int *&array, int size)
+{
+	srand(time(0));
+	array = new int[size];
+	size--;
+	while (size >= 0)
+		array[size] = rand() % 100 + 1, size--;
+}
 
 void swap(int &a, int &b)
 {
@@ -11,27 +22,34 @@ void swap(int &a, int &b)
 
 void BubbleSort(int *&arr,int size)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 1; i < size; i++)
 	{
-		for (int j = 0; j < size - 1; j++)
+		for (int j = 0; j < size - i; j++)
 		{
 			if (arr[j] > arr[j + 1])
 				swap(arr[j], arr[j + 1]);
 		}
 	}
 }
-
 void PrintArray(int *arr,int size)
 {
-	for (int i = 0; i<size; i++)
-		cout << arr[i] << ", ";
+	cout << "\nBubble Sort : ";
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
 int main()
 {
-	int *arr= new int [10] { 1,2121,123,3243,435546,54647,34,324,657,44};
-	BubbleSort(arr,10);
-	PrintArray(arr,10);
+	int size = 5;
+	int *arr;
+	initialize_array(arr, size);
+	cout << "-------Befor-------\n";
+	PrintArray(arr, size);
+	BubbleSort(arr,size);
+	cout << "\n-------After-------\n";
+	PrintArray(arr,5);
 	cout << endl;
+	delete[]arr;
 	system("pause");
 }
